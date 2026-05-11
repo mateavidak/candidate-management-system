16917bb71451c39010b8a8212dcb770f13578e9f
Matea Vidak <mtvidak@gmail.com>
2026-05-11 20:40:00 +0200

    feat: Implement models, DTOs and service layer

--- src/CandidateManagementSystem.Api/Program.cs
+++ src/CandidateManagementSystem.Api/Program.cs
@@ -1,10 +1,14 @@
 using CandidateManagementSystem.Api.Persistence;
+using CandidateManagementSystem.Api.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ISkillService, SkillService>();
+builder.Services.AddScoped<ICandidateService, CandidateService>();
 
 var app = builder.Build();
 
