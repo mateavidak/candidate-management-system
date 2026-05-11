6cf5227f781ae8fa4ed1865738b7cd1cc0757384
Matea Vidak <mtvidak@gmail.com>
2026-05-11 20:03:16 +0200

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="app">
+      <h1>Candidate Management</h1>
+      <p>React client — UI will be added here.</p>
+    </main>
+  )
+}
+
+export default App
